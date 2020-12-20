c6491f9031bcfe8c85be1988674ec998ddc54221
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-21 00:38:17 +0100

    Setting up all the routes files neccessary for the auth mechanism

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { json } from "body-parser";
 
+import { currentUserRoouter } from "./routes/current-user";
+import { signoutRouter } from "./routes/signout";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRoouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(signinRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
