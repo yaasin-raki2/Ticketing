7abd037e8dd20fd93c982477d6e7440f47d68154
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-21 18:51:34 +0100

    building a Error Hnadler Middleware to handle a DB , Request validation and Not Found Error. and installed "express-async-errors" package to handle async error throwing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { currentUserRoouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +16,12 @@ app.use(currentUserRoouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 app.use(signinRouter);
+
+app.get("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
@@ -12,16 +14,17 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 charachters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
 
     console.log("creating user...");
+    throw new DatabaseConnectionError();
 
     res.send({});
   }
