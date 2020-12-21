f0841fe5246cbc1f4001782111c93ef851dacf3b
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-21 21:13:59 +0100

    Setting Up Mongooseinside the auth service to connect with the Auth ClusterIP Service(K8S)

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 import { currentUserRoouter } from "./routes/current-user";
 import { signoutRouter } from "./routes/signout";
@@ -23,6 +24,21 @@ app.get("*", async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
 });
+
+start();
