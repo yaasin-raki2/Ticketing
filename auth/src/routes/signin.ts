438841e0238201ae58224a7c230970d0a031043a
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-25 14:20:16 +0100

    adding some basic validation to the signin route, then creating a validateRequest middleware to eliminate duplication inside signin and signup and future routes

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi there!");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must e valid"),
+    body("password").trim().notEmpty().withMessage("You must supply a password"),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {}
+);
 
 export { router as signinRouter };
