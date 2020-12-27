53390ee78e47295bdabbc3771281071af6300d6a
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-28 00:48:00 +0100

    Setting up Jester and mongo-memory-server

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -17,10 +17,10 @@ const start = async () => {
   } catch (err) {
     console.error(err);
   }
-};
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
-});
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
+};
 
 start();
