c49f684c5d824e9e46baf4e25815d8d0864ce8a8
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-28 02:03:16 +0100

    adding some basic signup route tests with superTest

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,42 @@ it("returns a 201 on successfull signup", async () => {
     })
     .expect(201);
 });
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "sdifyuqsgjksd",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid password", async () => {
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "1",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email or password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+    })
+    .expect(400);
+  return request(app)
+    .post("/api/users/signup")
+    .send({
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with missing email and password", async () => {
+  return request(app).post("/api/users/signup").send({}).expect(400);
+});
