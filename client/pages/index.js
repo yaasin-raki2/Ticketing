9c42fffef74012acaf7cf5935286353b96449a31
yaasin-raki2 <yassin23raki@gmail.com>
2020-12-29 20:38:33 +0100

    Solving the biggest Mystery of SSR in K8S when doing http requests inside getInitialProps when the site is being rendred inside the server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,27 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Home Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === "undefined") {
+    // We are on the server !
+    // Requests should be made to http://ingress-nginx.ingress-nginx.....
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // We are on the browser!
+    // Requests can be made with a base url of ""
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
+  }
+};
+
+export default LandingPage;
