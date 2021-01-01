2e7ad1e7aa550f0e95b9db5de932ae3c3498a232
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-01 23:51:20 +0100

    Adding the last test for the new route handler and adding it's implementation inside new.ts

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets for post requests", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -60,13 +61,23 @@ it("returns an error if an invalid price is provided", async () => {
 });
 
 it("create a ticket with valid inputs", async () => {
-  // Add in a check to make sure a ticket was saved
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const title = "title";
+  const price = 20;
+
   await request(app)
     .post("/api/tickets")
     .set("Cookie", global.signin())
     .send({
-      title: "title",
-      price: 20,
+      title,
+      price,
     })
-    .expect(200);
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
 });
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@yrtickets/common";
 
+import { Ticket } from "../models/ticket";
+
 const router = express.Router();
 
 router.post(
@@ -12,8 +14,17 @@ router.post(
     body("price").isFloat({ gt: 0 }).withMessage("Price must be greater than 0"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
