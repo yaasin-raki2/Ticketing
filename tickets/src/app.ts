c29703069409925e7235f330e5dcdc0b2f8972da
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-01 19:41:12 +0100

    adding some tests for the new route then creating a new route file and setting it up on both the route and the app.ts file so that it requres user authentication in order to move on to the /api/tickets url, then updating the global sign in to be a fake one so that it's no depending on the auth service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,9 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from "@yrtickets/common";
+import { errorHandler, NotFoundError, currentUser } from "@yrtickets/common";
+
+import { createTicketRouter } from "./routes/new";
 
 const app = express();
 
@@ -12,14 +14,12 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    // jest and superTest use plain HTTP request
-    // But the secure property in cookieSession when turned to true
-    // Accepts only HTTPS requests
-    // For this reason we gonna change the secure property to false
-    // When we are in a test environment
     secure: process.env.NODE_ENV !== "test",
   })
 );
+app.use(currentUser);
+
+app.use(createTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
