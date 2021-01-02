c508e185491f424a289ac8c9c42cd8cb4e28f4ac
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-02 01:15:37 +0100

    adding a console.error(err) to the error-handler middlware source code inside my common library so that we can debug errors that are  and republishing it

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -11,5 +11,8 @@ export const errorHandler = (
   if (err instanceof CustomError) {
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
+
+  console.error(err);
+
   res.status(400).send({ errors: [{ message: "Something went wrong" }] });
 };
