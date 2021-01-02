c071735e9b550e8dfec117341fc009a8b252c5f3
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-02 18:28:44 +0100

    Creating a update.test.ts file and adding tests to it , then creating a update.ts file and writing the tests implementation for the update route handler , the updating the global signin function to make sure it generates a diffrent id every time it gets called

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,112 @@
+import request from "supertest";
+import mongoose from "mongoose";
+
+import { app } from "../../app";
+
+it("returns 404 if the provided id does not exist", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "title",
+      price: 20,
+    })
+    .expect(404);
+});
+
+it("returns 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put(`/api/tickets/${id}`)
+    .send({
+      title: "title",
+      price: 20,
+    })
+    .expect(401);
+});
+
+it("returns 401 if the user does not own the ticket", async () => {
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "title",
+      price: 20,
+    })
+    .expect(201);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", global.signin())
+    .send({
+      title: "newTitle",
+      price: 30,
+    })
+    .expect(401);
+});
+
+it("returns 400 if the user provides an invalid title or price", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "title",
+      price: 20,
+    })
+    .expect(201);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "",
+      price: 30,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "newTitle",
+      price: -12,
+    })
+    .expect(400);
+});
+
+it("updates the ticket provided valid inputs", async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "title",
+      price: 20,
+    })
+    .expect(201);
+
+  const updatedResponse = await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: "newTitle",
+      price: 30,
+    })
+    .expect(200);
+
+  const ticketResponse = await request(app)
+    .get(`/api/tickets/${response.body.id}`)
+    .send()
+    .expect(200);
+
+  expect(response.body.title === updatedResponse.body.title).toBeFalsy();
+
+  expect(ticketResponse.body.title).toEqual(updatedResponse.body.title);
+  expect(ticketResponse.body.price).toEqual(updatedResponse.body.price);
+});
