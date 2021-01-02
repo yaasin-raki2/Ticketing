d82dc8b71eab752ed7badf6117814efa4483bbc1
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-02 01:31:11 +0100

    Updating my common library inside both the Auth and the Tickets service and fixing the invalid id error inside the show.test.ts file

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,9 +1,12 @@
 import request from "supertest";
+import mongoose, { mongo } from "mongoose";
 
 import { app } from "../../app";
 
 it("returns a 404 if the ticket is not found", async () => {
-  await request(app).get("/api/tickets/fakeid").send().expect(404);
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).get(`/api/tickets/${id}`).send().expect(404);
 });
 
 it("returns the ticket if the ticket is found", async () => {
