d01f9581de6a94604e172bdddaf67cdc773815c0
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-02 01:10:09 +0100

    Creating a show route handler and addig some logic to retrieve a ticket from DB so we passed the second test . unfortunately we couldn't pass the first test due to a 400 error which turned out to be a invalid id error , and now trying to fix it , even though I don't know how '-'

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@yrtickets/common";
 
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.use(
 app.use(currentUser);
 
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
