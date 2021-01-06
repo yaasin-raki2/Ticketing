6500d23f822a974968df7521f55ad3e6a8efd120
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-06 16:47:19 +0100

    Created a ticket-created-publisher.ts file which is a class that extends the base publisher , then adding logic to send this data inside the new.ts route handler but still didn't connect node nats streaming server client to this instance

---
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketCreatedEvent } from "@yrtickets/common";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+}
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@yrtickets/common";
 
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -23,6 +24,14 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    // Send data to Nats so that other services can recieve it as an event
+    await new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: req.currentUser!.id,
+    });
 
     res.status(201).send(ticket);
   }
