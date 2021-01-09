2b4f7c18f0117840644efa56959b261bd0e94b3e
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-09 12:52:02 +0100

    Setting up some basic routes for the order service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@yrtickets/common";
 
+import { newOrderRouter } from "./src/new";
+import { indexOrderRouter } from "./src";
+import { showOrderRouter } from "./src/show";
+import { deleteOrderRouter } from "./src/delete";
+
 const app = express();
 
 app.set("trust proxy", true);
@@ -16,6 +21,11 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(newOrderRouter);
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
