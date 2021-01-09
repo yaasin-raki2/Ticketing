b61224433bd3f77794b9ccfda4b1ddf4fc4bfe22
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-09 19:18:27 +0100

    adding some basic validation to the new.ts route handler

--- orders/src/src/new.ts
+++ orders/src/src/new.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+import { body } from "express-validator";
+import { requireAuth, validateRequest } from "@yrtickets/common";
 
 const router = express.Router();
 
-router.post("/api/orders", (req: Request, res: Response) => {
-  res.send();
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    res.send();
+  }
+);
 
 export { router as newOrderRouter };
