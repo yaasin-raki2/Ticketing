48c666f84df7157a18f27bbab097d0f88754caa9
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-12 23:08:38 +0100

    finishing up the delete.ts route handler

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,42 @@
 import express, { Request, Response } from "express";
+import { param } from "express-validator";
+import mongoose from "mongoose";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+} from "@yrtickets/common";
+
+import { Order, OrderStatus } from "../models/orders";
 
 const router = express.Router();
 
-router.delete("/api/orders/:id", (req: Request, res: Response) => {
-  res.send();
-});
+router.delete(
+  "/api/orders/:id",
+  requireAuth,
+  [
+    param("id")
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("orderId must be valid"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id).populate("ticket");
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
