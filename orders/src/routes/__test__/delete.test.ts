27952a142f65dc6fb18bf5dd063b7cdab2439e55
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-19 23:26:53 +0100

    adding publishing tests for the new and delete route handelers

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import { app } from "../../app";
 import { Ticket } from "../../models/ticket";
 import { OrderStatus } from "../../models/orders";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns 400 if provided invalid id", async () => {
   await request(app)
@@ -74,4 +75,28 @@ it("Marks an order as cancelled", async () => {
   expect(response.body.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("Emits an order cancelled event");
+it("Emits an order cancelled event", async () => {
+  const user = global.signin();
+
+  const ticket = Ticket.build({
+    title: "title",
+    price: 20,
+  });
+  await ticket.save();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -5,6 +5,7 @@ import { app } from "../../app";
 
 import { Order, OrderStatus } from "../../models/orders";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns 401 if the user is not authenticated", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -96,4 +97,20 @@ it("reserves a ticket", async () => {
   expect(response.body.ticket.price).toEqual(20);
 });
 
-it.todo("emits an order:created event");
+it("emits an order:created event", async () => {
+  const ticket = Ticket.build({
+    title: "title",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({
+      ticketId: ticket.id,
+    })
+    .expect(201);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
