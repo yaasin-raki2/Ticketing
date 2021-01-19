2a4101b5fc5f13db80958ec5989eed0d75e96de9
yaasin-raki2 <yassin23raki@gmail.com>
2021-01-19 20:45:28 +0100

    Publishing the order creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -11,6 +11,9 @@ import {
 
 import { Order } from "../models/orders";
 import { Ticket } from "../models/ticket";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { OrderCancelledPublisher } from "../events/publishers/order-cancelled-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -67,6 +70,16 @@ router.post(
     await order.save();
 
     // Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
