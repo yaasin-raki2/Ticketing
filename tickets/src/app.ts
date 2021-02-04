fc9c5f6607306cfc0db10fdb139640be5c8a8d35
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-04 15:14:32 +0100

    creating tickets listeners instances inside index.ts and fixing the 3 days silly bug at app.ts -_-

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,11 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@yrtickets/common";
 
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
+import { updateTicketRouter } from "./routes/update";
+
 const app = express();
 
 app.set("trust proxy", true);
@@ -16,6 +21,11 @@ app.use(
   })
 );
 app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
