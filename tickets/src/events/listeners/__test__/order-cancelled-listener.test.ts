daa83fe10ec3385de36e92a83d535a90a073c51a
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-06 16:59:19 +0100

    Creating an OrderCancelledListener sub-class and testing it with jest

---
+++ tickets/src/events/listeners/__test__/order-cancelled-listener.test.ts
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { OrderCancelleddEvent } from "@yrtickets/common";
+
+import { Ticket } from "../../../models/ticket";
+import { natsWrapper } from "../../../nats-wrapper";
+import { OrderCancelledListener } from "../order-cancelled-listener";
+
+const setup = async () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const orderId = new mongoose.Types.ObjectId().toHexString();
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+    userId: "asdf",
+  });
+  ticket.set({ orderId });
+  await ticket.save();
+
+  const data: OrderCancelleddEvent["data"] = {
+    id: orderId,
+    version: 0,
+    ticket: {
+      id: ticket.id,
+    },
+  };
+
+  //@ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, ticket, data, msg };
+};
+
+it("updates the ticket, publishes the event, and acks the message", async () => {
+  const { listener, ticket, data, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+
+  const updatedTicket = await Ticket.findById(ticket.id);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+  expect(updatedTicket.orderId).toBeUndefined();
+  expect(msg.ack).toHaveBeenCalled();
+});
---
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,34 @@
+import { Message } from "node-nats-streaming";
+import { Listener, Subjects, OrderCancelleddEvent } from "@yrtickets/common";
+
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelleddEvent> {
+  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelleddEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    });
+
+    msg.ack();
+  }
+}
