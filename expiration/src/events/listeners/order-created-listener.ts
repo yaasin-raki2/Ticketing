866680a3a704ba3e5c5299d5f9abfebb0276f67d
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-07 19:43:26 +0100

    Creating a base OrderCreatedListener sub-class and a base expiration -job-queue

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,18 @@
+import { Message } from "node-nats-streaming";
+import { Listener, OrderCreatedEvent, Subjects } from "@yrtickets/common";
+
+import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+
+    msg.ack();
+  }
+}
