cca02fc82abb7629aaed2e682a530c9e4530c87f
yaasin-raki2 <yassin23raki@gmail.com>
2021-02-08 13:26:44 +0100

    wring up the OrderCreatedListener sub-class instance to the index.ts file and adding the delay processing logic to this sub-class

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -9,9 +9,17 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName;
 
   async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    });
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log("waiting this many milliseconds to process the job : ", delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    );
 
     msg.ack();
   }
